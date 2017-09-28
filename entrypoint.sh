a42c6256373694b08b7e7a6ee639831195edd3a4
Peter Wu <piterwu@outlook.com>
2017-09-28 21:47:47 +0800

    文件夹权限

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 groupmod -o -g $AUDIO_GID audio
 groupmod -o -g $VIDEO_GID video
-if [ $UID != $(echo `id -u wechat`) -o $GID != $(echo `id -g wechat`) ]; then
+if [ $GID != $(echo `id -g wechat`) ]; then
     groupmod -o -g $GID wechat
-    if [ $UID != $(echo `id -u wechat`) ]; then
-        usermod -o -u $UID wechat
-    fi
-    chown -R wechat:wechat /WeChatFiles
 fi
+if [ $UID != $(echo `id -u wechat`) ]; then
+    usermod -o -u $UID wechat
+fi
+chown wechat:wechat /WeChatFiles
 
 su wechat <<EOF
 if [ "$1" ]; then
